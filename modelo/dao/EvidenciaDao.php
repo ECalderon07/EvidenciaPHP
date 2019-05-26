d17ad47da15b14cc14fba65e5f75ec838d87795a
Edwin Calderon <eicalderon@misena.edu.co>
2019-05-26 13:01:53 -0500

    Se modifico archivo Dao

--- modelo/dao/EvidenciaDao.php
+++ modelo/dao/EvidenciaDao.php
@@ -2,11 +2,32 @@
 
 namespace modelo\dao;
 
-/**
- * Description of EvidenciaDao
- *
- * @author Edwin
- */
-class EvidenciaDao {
-    //put your code here
+use PDO;
+use modelo\generico\GenericoDAO;
+use modelo\vo\EvidenciaVO;
+
+class EvidenciaDao extends GenericoDAO {
+
+    private $evidenciaVO;
+
+    public function __construct($cnn) {
+        parent::__construct($cnn, 'evidencia'); //primer parámetro la conexión/2do parámetro la tabla
+        $this->libroVO = new LibroVO();
+    }
+
+    public function listarEvidencia() {
+        $sentencia = "SELECT * FROM $this->tabla ";
+        $resultado = $this->cnn->prepare($sentencia);
+        $resultado->execute();
+        return $resultado->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function consultarLibro($id) {
+        $sentencia = "SELECT * FROM $this->tabla WHERE idEvidencia=:idEvidencia";
+        $resultado = $this->cnn->prepare($sentencia);
+        $resultado->bindValue(':idEvidencia', $isbn);
+        $resultado->execute();
+        return $resultado->fetch(PDO::FETCH_OBJ);
+    }
+
 }
