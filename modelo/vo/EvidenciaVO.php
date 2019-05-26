1036812beba965afeb5e508f2b6ac700e02d732f
Edwin Calderon <eicalderon@misena.edu.co>
2019-05-26 12:41:15 -0500

    Se agrego el archivo Vo

--- modelo/vo/EvidenciaVO.php
+++ modelo/vo/EvidenciaVO.php
@@ -1,18 +1,58 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace modelo\vo;
 
-/**
- * Description of EvidenciaVO
- *
- * @author Edwin
- */
-class EvidenciaVO {
-    //put your code here
+use modelo\generico\IGenericoVO;
+
+class EvidenciaVO implements IGenericoVO {
+
+    private $idEvidencia;
+    private $ruta;
+    private $cedula;
+
+    function __construct() {
+        
+    }
+
+    function getIdEvidencia() {
+        return $this->idEvidencia;
+    }
+
+    function getRuta() {
+        return $this->ruta;
+    }
+
+    function getCedula() {
+        return $this->cedula;
+    }
+
+    function setIdEvidencia($idEvidencia) {
+        $this->idEvidencia = $idEvidencia;
+    }
+
+    function setRuta($ruta) {
+        $this->ruta = $ruta;
+    }
+
+    function setCedula($cedula) {
+        $this->cedula = $cedula;
+    }
+
+    public function convertir($info) {
+        $atributos = array_keys(get_object_vars($this));
+        foreach ($atributos as $nombreAtributo) {
+            if (isset($info['pro_' . $nombreAtributo])) {
+                $this->$nombreAtributo = $info['pro_' . $nombreAtributo];
+            }
+        }
+    }
+
+    public function getAtributos() {
+        $atributos['idEvidencia'] = $this->idEvidencia;
+        $atributos['ruta'] = $this->ruta;
+        $atributos['cedula'] = $this->cedula;
+
+        return $atributos;
+    }
+
 }
