7968fe29bcc5fe9233c6d7a3eed34356176bd993
Edwin Calderon <eicalderon@misena.edu.co>
2019-05-26 21:25:16 -0500

    Se modifico 1ra vez registrarEvidencia.php

--- vista/evidencia/registrarArchivo.php
+++ vista/evidencia/registrarArchivo.php
@@ -1,17 +1,59 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+
+<body>
+    <form id="formCargaArchivo" enctype="multipart/form-data">
+        <input type="txt" name="inpCedula" id="inpCedula">
+        <input type="file" name="inpArchivo" id="inpArchivo">
+        <input type="submit" name="inpSubir" id="inpSubir" value="SUBIR" disabled="disabled">
+    </form>
+</body>
+
+<script>
+
+
+    //desahabilitar la funciòn por defecto del form
+    $("#formCargaArchivo").submit(function () {
+        return false;
+    })
+
+    //observar el cambio de archivo 
+    $("#inpArchivo").change(function () {
+        console.log($(this)[0].files[0])
+        var nombre = $(this)[0].files[0].name
+        var tamaño = $(this)[0].files[0].size
+        var tipo = nombre.split(".")
+        tipo = tipo[1]
+        console.log(nombre + " " + tipo + " " + " " + tamaño)
+
+        if (tipo == 'jpg' && (tamaño >= 0 && tamaño < 2145728)) {
+            $("#inpSubir").removeAttr("disabled", "disabled")
+        } else {
+            $("#inpSubir").attr("disabled", "disabled")
+            alert("Archivo no vàlido")
+        }
+
+
+    })
+
+    //subir archivo
+    $("#inpSubir").click(function () {
+        alert("prueba")
+        var formulario = new FormData($("#formCargaArchivo")[0])
+        $.ajax({
+            url: 'cargarArchivo.php',
+            data: formulario,
+            type: 'post',
+            dataType: 'json',
+            processData: false,
+            cache: false,
+            contentType: false,
+            success: function (resultado) {
+                console.log(resultado)
+                alert("correcto")
+            },
+            error: function () {
+                alert("falla")
+            }
+        })
+    })
+</script>
+
