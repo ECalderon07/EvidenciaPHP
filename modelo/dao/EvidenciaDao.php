f75aa6a6c0dc136164e2f689d2475ae47af9457a
Edwin Calderon <eicalderon@misena.edu.co>
2019-05-26 20:15:09 -0500

    Se modifico archivo Dao 2vez

--- modelo/dao/EvidenciaDao.php
+++ modelo/dao/EvidenciaDao.php
@@ -12,22 +12,23 @@ class EvidenciaDao extends GenericoDAO {
 
     public function __construct($cnn) {
         parent::__construct($cnn, 'evidencia'); //primer parámetro la conexión/2do parámetro la tabla
-        $this->libroVO = new LibroVO();
+        $this->evidenciaVO = new EvidenciaVO();
     }
 
-    public function listarEvidencia() {
-        $sentencia = "SELECT * FROM $this->tabla ";
+    public function registrarArchivo($cedula,$random) {
+        $sentencia = "INSERT INTO $this->tabla (ruta, cedula) VALUES (:ruta,:cedula)";
+        $resultado = $cnn->prepare($sentencia);
+        $resultado->bindValue(':ruta', "./evidencia/$cedula/$random.jpg");
+        $resultado->bindValue(':cedula', $cedula);
+        $resultado->execute();
+    }
+
+    public function listarArchivo($cedula) {
+        $sentencia = "SELECT * FROM $this->tabla WHERE cedula=:cedula";
         $resultado = $this->cnn->prepare($sentencia);
+        $resultado->bindValue(':cedula', $cedula);
         $resultado->execute();
         return $resultado->fetchAll(PDO::FETCH_OBJ);
-    }
-
-    public function consultarLibro($id) {
-        $sentencia = "SELECT * FROM $this->tabla WHERE idEvidencia=:idEvidencia";
-        $resultado = $this->cnn->prepare($sentencia);
-        $resultado->bindValue(':idEvidencia', $isbn);
-        $resultado->execute();
-        return $resultado->fetch(PDO::FETCH_OBJ);
     }
 
 }
