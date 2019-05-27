983451fbfb3ef810f2199e0916f146ec1b8c88e0
Edwin Calderon <eicalderon@misena.edu.co>
2019-05-26 21:24:43 -0500

    Se modifico listarEvidencia.php

--- vista/evidencia/listarArchivo.php
+++ vista/evidencia/listarArchivo.php
@@ -1,17 +1,44 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>ruta</th>
+            <th>Ver</th>
+            <th>Descargar</th>
+            <th>Eliminar</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>        
+</table>
+</body>
+<script>
+    $("#btnListarEvidencia").click(function () {
+
+        var inpCedula = $("#inpCedula").val();
+        $.ajax({
+            url: 'cargarArchivo.php',
+            data: {inpCedula: inpCedula},
+            type: 'post',
+            dataType: 'json',
+            success: function (resultado) {
+                console.log(resultado)
+                alert("correcto")
+                var acum = ""
+                $.each(resultado.mensaje, function (fila, valor) {
+                    acum += "<tr>";
+                    acum += "<td>" + valor.idEvidencia + "</td><td><img src='" + valor.ruta + "' /></td></td><button></button><td><a href='" + valor.ruta + "'>VER</a></td><td><button><a href='" + valor.ruta + "' download>DESCARGAR</a></button></td><td><button>eliminar</button></td>"
+                    acum += "</tr>"
+                })
+                $("table tbody").html(acum)
+
+            },
+            error: function () {
+                alert("falla")
+            }
+        })
+    })
+</script>
+
